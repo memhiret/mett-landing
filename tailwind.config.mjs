56caa930d65651299fd601e67236f4e13a7286af
yonit1dev <yonit.anosie@gmail.com>
2024-11-20 17:33:06 +0300

    Inital Commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        memhirETBlue: {
+          500: "#153d59",
+          700: "#1b3e98",
+        },
+        memhirETWhite: {
+          500: "#F8F9F5",
+        },
+        memhirETBlack: {
+          300: "#1E1E1E",
+          500: "#1E2328",
+        },
+        memhirETYellow: {
+          500: "#f2b41c",
+        },
+      },
+      fontFamily: {
+        bauhaus: ["Bauhaus", "sans-serif"],
+        body: ["DM Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
