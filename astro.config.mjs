090a716d019368835f2ff1a45d212e477ad3c9fb
yonit1dev <yonit.anosie@gmail.com>
2024-11-20 17:42:32 +0300

    Fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind()],
+  vite: {
+    build: {
+      assetsInlineLimit: 0,
+    },
+  },
 });
