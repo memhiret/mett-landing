da4aed7e9ce16dcb8b9cc6445ea36d57f3b12c4b
yonit1dev <yonit.anosie@gmail.com>
2024-12-18 18:44:49 +0300

    Feat: Terms and Conditions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://mett.memhiret.com",
+  integrations: [tailwind(), sitemap(), robotsTxt()],
   vite: {
     build: {
       assetsInlineLimit: 0,
